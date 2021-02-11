df10ad218c2bd630a093b7f46f4c5f67fae80ac1
Belinda Brown <belindabrownr04@gmail.com>
2021-02-11 17:51:44 -0600

    77% Cap 1 (are 4 caps)

---
+++ dslevel_1/python_intro.py.py
@@ -0,0 +1,69 @@
+  #--  --  --  --  Introduction to Python
+# Used for Data Scientist Training Path 
+#FYI it's a compilation of how to work
+#with different commands.
+
+## -----> Calculations with variables
+### --------------------------------------------------------
+# Division
+print(5 / 8)
+
+#Addition
+print(7 + 10)
+
+### --------------------------------------------------------
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+savings = 100
+factor = 1.10
+years = 7
+result = (savings * factor ** years)
+print(result)
+
+## -----> Variables and Types 
+### --------------------------------------------------------
+# Create a variable savings
+savings = 100 
+
+# Print out savings
+print(savings)
+
+### --------------------------------------------------------
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+years = 7
+# Calculate result
+result = savings*growth_multiplier**years
+
+# Print out result
+print(result)
+
+### --------------------------------------------------------
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+### --------------------------------------------------------
+type(a)
+#Out[1]: float
+type(b) 
+#Out[2]: str
+type(c)
+#Out[3]: bool
+
+### --------------------------------------------------------
+
