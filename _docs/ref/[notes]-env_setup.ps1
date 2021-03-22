ba4d63e8901b9b03e53d07aa1513c470ec632227
Belinda Brown <belindabrownr04@gmail.com>
2021-03-22 08:42:35 -0600

    Update and rename setup_tips.ps1 to [notes]-env_setup.ps1

--- _docs/ref/[notes]-env_setup.ps1
+++ _docs/ref/[notes]-env_setup.ps1
@@ -1,6 +1,5 @@
-# This is my compilations of 
-# several tips for the 
-# Env install -> Power Shell 
+# Environment Installation 
+## ----->                 Power Shell 
 
 # to print all the packages 
 # of the environment and saved it 
@@ -18,13 +17,11 @@ conda env update -f file_with_versions.yml --debug
 # to create a env with a single command 
 conda env create --name environment_name --file="path/file_with_versions.yml" --name environment_name -v --force
 
-
 # show version of a package 
 pip show package_name
 
 # clone env with a file 
 conda create --name environment_name --clone azureml_py36 --debug
-
 
 
 # Fix some problems with scikit-learn 
